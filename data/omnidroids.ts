221f7584fd68f1718e65b23997826c0d8259c9c0
MrMickey98-git <babyblue92b@gmail.com>
2025-11-30 20:07:53 -0800

    Rename omnidroids with new slugs and names

--- data/omnidroids.ts
+++ data/omnidroids.ts
@@ -1,23 +1,23 @@
 import { Omnidroid } from '@/types'
 
 export const omnidroids: Array<Omnidroid> = [
-  { slug: 'X1', name: 'Omnidroid v.X1', description: 'Threaded locomotion, sensory display, bi-articulated grappling claws', img: 'x1.png' },
+  { slug: '01', name: 'Omnidroid 01', description: 'Threaded locomotion, sensory display, bi-articulated grappling claws', img: 'x1.png' },
   {
-    slug: 'X2',
-    name: 'Omnidroid v.X2',
+    slug: '02',
+    name: 'Omnidroid 02',
     description: 'Bipedal locomotion, omni-directional sensory array, bi-articulated grappling claws',
     img: 'x2.png',
   },
   {
-    slug: 'X3',
-    name: 'Omnidroid v.X3',
+    slug: '03',
+    name: 'Omnidroid 03',
     description: 'Suspended tri-pedal locomotion, grappling-claws, uni-directional sensory array',
     img: 'x3.png',
   },
-  { slug: 'X4', name: 'Omnidroid v.X4', description: 'Quadra-pedal locomotion, grappling-claws, quadra-directional sensory array', img: 'x4.png' },
-  { slug: 'X5', name: 'Omnidroid v.X5', description: 'Quadra-pedal locomotion, omni articulated grappling-claws', img: 'x5.png' },
-  { slug: 'X6', name: 'Omnidroid v.X6', description: 'Quadra-pedal locomotion, omni articulated grappling-claws', img: 'x6.png' },
-  { slug: 'X7', name: 'Omnidroid v.X7', description: 'Penta-pedal locomotion, trans universal articulation, sensor cluster', img: 'x7.png' },
-  { slug: 'X8', name: 'Omnidroid v.X8', description: 'Omni-directional, trans universal articulation, bi-polar sensor clusters', img: 'x8.png' },
-  { slug: 'X9', name: 'Omnidroid v.X9', description: 'Omni-directional, trans-universal articulation, bi-polar sensor cluster', img: 'x9.png' },
+  { slug: '04', name: 'Omnidroid 04', description: 'Quadra-pedal locomotion, grappling-claws, quadra-directional sensory array', img: 'x4.png' },
+  { slug: '05', name: 'Omnidroid 05', description: 'Quadra-pedal locomotion, omni articulated grappling-claws', img: 'x5.png' },
+  { slug: '06', name: 'Omnidroid 06', description: 'Quadra-pedal locomotion, omni articulated grappling-claws', img: 'x6.png' },
+  { slug: '07', name: 'Omnidroid 07', description: 'Penta-pedal locomotion, trans universal articulation, sensor cluster', img: 'x7.png' },
+  { slug: '08', name: 'Omnidroid 08', description: 'Omni-directional, trans universal articulation, bi-polar sensor clusters', img: 'x8.png' },
+  { slug: '09', name: 'Omnidroid 09', description: 'Omni-directional, trans-universal articulation, bi-polar sensor cluster', img: 'x9.png' },
 ]
